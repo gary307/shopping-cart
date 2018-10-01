5a872b6d25776a8cc362071c91e737ad5b55bed9
Gary Butler <gary.butler@karmarama.com>
2018-10-01 20:52:01 +0100

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 3000,
-    compress: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader'
-      }
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        compress: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    })
-  ]
 };
